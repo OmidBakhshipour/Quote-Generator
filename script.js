12d43ba655df339b3597fb276020536e24c42ab7
Omid BakhshiPour <bakhshipouromid@gmail.com>
2022-01-13 14:45:26 +0330

    Manipulating DOM to show quotes

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const quoteAuthor = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if author is blank
+    if(!quote.author) {
+        quoteAuthor.textContent = 'Unknown';
+    } else {
+        quoteAuthor.textContent = quote.author;
+    }
+    // Check quote length
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
+
+
 async function getQuotes() {
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
+    const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await axios.get(apiUrl);
-        console.log(response.data);
+        apiQuotes = response.data;
+        newQuote();
 
     } catch (error) {
         console.log('No quotes!', error);
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// EventListeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load 
 getQuotes();
